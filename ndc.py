985d782630c355fbe79a2ed977fc111d6321bd7e
ferreiralourenco <ferreiralourenco@VAN-GOGH.LOCAL>
2023-05-26 10:53:49 +0200

    update

--- ndc.py
+++ ndc.py
@@ -1,1 +1,15 @@
 import pyxel
+
+class Game:
+    def __init__(self):
+        pyxel.init(128, 128, title="Best game for ever")
+    
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        print("test")
+
+    def draw(self):
+        pyxel.rect(80, 80, 20, 20, 1)
+
+Game()
